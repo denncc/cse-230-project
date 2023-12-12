4c2d4c61737aa1ca138606a7a5b6a7859ccbe0ea
ViYulton <brookssxj@gmail.com>
2023-12-11 18:24:48 -0800

    Change from simpleMain to defaultMain

--- src/Game.hs
+++ src/Game.hs
@@ -1,3 +1,29 @@
 module Game where
 
-type Game=[Int]
+type Element= Maybe Int
+type Row=[Element]
+type Grid=[Row]
+
+data Game = Game {
+  cursor :: (Int, Int),
+  grid :: Grid,
+  previous :: Maybe Game
+}
+
+demoGrid :: Grid
+demoGrid = [[Just 3, Just 6, Just 5, Just 5, Just 7, Just 1, Just 2, Just 5, Just 1],
+    [Just 3, Just 6, Just 5, Just 5,Nothing, Just 1, Just 2, Just 5,Just 5],
+    [Just 3, Just 6, Nothing, Just 5,Just 7, Just 1, Just 2, Just 5,Just 5],
+    [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5],
+    [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5],
+    [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1,Just 3, Just 5, Just 5],
+    [Just 3, Just 6, Just 5,Just 5,Just 7,Just 1, Just 2, Just 5, Just 5],
+    [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5],
+    [Just 3, Just 6, Just 5,Just 5,Just 7, Just 1, Just 2, Just 5,Just 5]]
+
+demo :: Game
+demo = Game {
+  cursor = (4, 4),
+  grid = demoGrid,
+  previous = Nothing
+}
--- src/UI.hs
+++ src/UI.hs
@@ -1,22 +1,68 @@
 module UI where
 
-import Brick (Widget, (<+>) , (<=>), str, withBorderStyle, joinBorders)
+import Brick
+import Brick.Main (App, defaultMain, simpleMain, neverShowCursor)
+import Brick.Widgets.Core (joinBorders, withBorderStyle, (<+>) , (<=>), str)
 import Brick.Widgets.Center (center, hCenter)
 import Brick.Widgets.Border (borderWithLabel, vBorder)
 import Brick.Widgets.Border.Style (unicode)
-import Game(Game)
+import Brick.Widgets.Table (table, renderTable)
+import qualified Graphics.Vty as V
+import Lens.Micro
+import Game
+
+styleCursor, styleCellGiven, styleCellInput, styleCellNote :: AttrName
+styleSolved, styleUnsolved :: AttrName
+styleCursor    = attrName "styleCursor"
+styleCellGiven = attrName "styleCellGiven"
+styleCellInput = attrName "styleCellInput"
+styleCellNote  = attrName "styleCellNote"
+styleSolved    = attrName "styleSolved"
+styleUnsolved  = attrName "styleUnsolved"
+
+attributes :: AttrMap
+attributes = attrMap V.defAttr
+  [ (styleCursor    , bg V.brightBlack)
+  , (styleCellGiven , V.defAttr)
+  , (styleCellInput , fg V.blue)
+  , (styleCellNote  , fg V.yellow)
+  , (styleSolved    , fg V.green)
+  , (styleUnsolved  , fg V.red)
+  ]
+
+handleEvent :: BrickEvent () e -> EventM () Game ()
+handleEvent be = halt 
 
 drawBoard :: Game -> Widget ()
-drawBoard game = borderWithLabel (str "Board") $ center (str "board")
+drawBoard game = borderWithLabel (str "Board") $ renderTable $ table $ map (map mapElement) $ grid game
+  where
+    mapElement :: Element -> Widget ()
+    mapElement Nothing = str " "
+    mapElement (Just v) = str $ show v
 
 drawInstruction :: Widget ()
 drawInstruction = borderWithLabel (str "Instruction") $ hCenter (str "Use ↑, ↓, ←, → to move cursor") <=> hCenter (str "Use 1-9 to fill the selected cell") <=> hCenter (str "Use Backspace to clear number in the selected cell")
 
 drawStatus :: Widget ()
-drawStatus = borderWithLabel (str "Status") $ hCenter (str "Cursor: ") <=> hCenter (str "Progress: ") 
+drawStatus = borderWithLabel (str "Status") $ hCenter (str "Cursor: (1,1)") <=> hCenter (str "Progress: 10%")
 
 drawResult :: Widget ()
 drawResult = borderWithLabel (str "Result") $ hCenter (str "Solution found")
 
-ui :: Game -> Widget ()
-ui game = drawBoard game <+> (drawInstruction <=> drawStatus <=> drawResult)
+drawUI :: Game -> Widget ()
+drawUI game = drawBoard game <+> (drawInstruction <=> drawStatus <=> drawResult)
+
+app :: App Game () ()
+app = App 
+  {
+    appDraw = \s -> [drawUI s]
+  , appChooseCursor = neverShowCursor
+  , appHandleEvent  = handleEvent
+  , appStartEvent   = return ()
+  , appAttrMap      = const attributes
+  }
+
+main :: IO ()
+main = do
+  endGame <- defaultMain app demo
+  return ()
