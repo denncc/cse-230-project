616e1dc250bc36b679e235c37e9e0d1528361575
ViYulton <brookssxj@gmail.com>
2023-12-12 11:46:30 -0800

    Add keyboard interaction

--- src/Game.hs
+++ src/Game.hs
@@ -1,4 +1,5 @@
 module Game where
+import Prelude hiding (Right, Left)
 
 type Element= Maybe Int
 type Row=[Element]
@@ -9,6 +10,49 @@ data Game = Game {
   grid :: Grid,
   previous :: Maybe Game
 }
+
+data Direction = Up | Down | Left | Right
+
+resetGame :: Game -> Game
+resetGame game = Game { 
+  cursor = cursor game,
+  grid = map (map f) $ grid game,
+  previous = Just game }
+  where
+    f _ = Nothing
+
+wrap :: Int -> Int
+wrap n
+  | n>=9 = n-9
+  | n<0 = n+9
+  | otherwise = n
+
+moveCursor :: Direction -> Int -> Game -> Game
+moveCursor dir dist game = game { cursor = move dir dist $ cursor game }
+  where
+    move Up dist (x,y) = (wrap $ x-dist, y)
+    move Down dist (x,y) = (wrap $ x+dist, y)
+    move Left dist (x,y) = (x, wrap $ y-dist)
+    move Right dist (x,y) = (x, wrap $ y+dist)
+
+modifyCell val game = Game {
+  cursor = cursor game,
+  grid = modifyRow (cursor game) val $ grid game,
+  previous = Just game
+}
+  where
+    modifyRow (x,y) val (r:rs) = case x of 
+      0 -> modifyCol y val r:rs
+      _ -> r:modifyRow (x-1,y) val rs
+      where
+        modifyCol col val (c:cs) = case col of
+          0 -> case val of 
+            0 -> Nothing 
+            _ -> Just val
+            :cs
+          _ -> c:modifyCol (col-1) val cs
+        modifyCol _ _ _ = []
+    modifyRow _ _ _ = []
 
 demoGrid :: Grid
 demoGrid = [[Just 3, Just 6, Just 5, Just 5, Just 7, Just 1, Just 2, Just 5, Just 1],
--- src/UI.hs
+++ src/UI.hs
@@ -31,7 +31,33 @@ attributes = attrMap V.defAttr
   ]
 
 handleEvent :: BrickEvent () e -> EventM () Game ()
-handleEvent be = halt 
+handleEvent (VtyEvent (V.EvKey key [V.MCtrl])) = do
+  game <- get
+  case key of
+    V.KChar 'c' -> halt
+    V.KChar 'z' -> maybe continueWithoutRedraw put (previous game)
+    V.KChar 'r' -> put $ resetGame game
+    _ -> continueWithoutRedraw
+handleEvent (VtyEvent (V.EvKey key [])) = do
+  game <- get
+  put $ case key of
+    -- Move by cell
+    V.KUp       -> moveCursor Game.Up 1 game
+    V.KDown     -> moveCursor Game.Down 1 game
+    V.KLeft     -> moveCursor Game.Left 1 game
+    V.KRight    -> moveCursor Game.Right 1 game
+    -- Modify Cell
+    V.KChar '1' -> modifyCell 1 game
+    V.KChar '2' -> modifyCell 2 game
+    V.KChar '3' -> modifyCell 3 game
+    V.KChar '4' -> modifyCell 4 game
+    V.KChar '5' -> modifyCell 5 game
+    V.KChar '6' -> modifyCell 6 game
+    V.KChar '7' -> modifyCell 7 game
+    V.KChar '8' -> modifyCell 8 game
+    V.KChar '9' -> modifyCell 9 game
+    V.KBS       -> modifyCell 0 game
+handleEvent _ = continueWithoutRedraw
 
 drawBoard :: Game -> Widget ()
 drawBoard game = borderWithLabel (str "Board") $ renderTable $ table $ map (map mapElement) $ grid game
@@ -43,17 +69,17 @@ drawBoard game = borderWithLabel (str "Board") $ renderTable $ table $ map (map 
 drawInstruction :: Widget ()
 drawInstruction = borderWithLabel (str "Instruction") $ hCenter (str "Use ↑, ↓, ←, → to move cursor") <=> hCenter (str "Use 1-9 to fill the selected cell") <=> hCenter (str "Use Backspace to clear number in the selected cell")
 
-drawStatus :: Widget ()
-drawStatus = borderWithLabel (str "Status") $ hCenter (str "Cursor: (1,1)") <=> hCenter (str "Progress: 10%")
+drawStatus :: (Int, Int) -> Widget ()
+drawStatus cursor = borderWithLabel (str "Status") $ hCenter (str $ "Cursor: " ++ show cursor) <=> hCenter (str "Progress: 10%")
 
 drawResult :: Widget ()
 drawResult = borderWithLabel (str "Result") $ hCenter (str "Solution found")
 
 drawUI :: Game -> Widget ()
-drawUI game = drawBoard game <+> (drawInstruction <=> drawStatus <=> drawResult)
+drawUI game = drawBoard game <+> (drawInstruction <=> drawStatus (cursor game) <=> drawResult)
 
 app :: App Game () ()
-app = App 
+app = App
   {
     appDraw = \s -> [drawUI s]
   , appChooseCursor = neverShowCursor
